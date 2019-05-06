bc24460b2a706b424d4beabcd39d17038c18bc41
Marco Galatola <marco.galatola@di.unito.it>
2019-05-06 18:41:02 +0200

    addPayment comunica con SnaporazSpring

--- SnaporazJ2EE-ejb/src/main/java/ejb/PaymentManager.java
+++ SnaporazJ2EE-ejb/src/main/java/ejb/PaymentManager.java
@@ -10,6 +10,13 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.json.JsonObject;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
@@ -23,19 +30,38 @@ public class PaymentManager implements PaymentManagerLocal {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-
     @Override
-    public String addPayment(String id, String userId, long project, double amount){
-        Map<String, Payment> responseMap = new HashMap<>();
-        for(Payment p : paymentFacade.findAll())
-            if(p.getId().equals(id))
-                return "Errore: Transazione già effettuata";
-        Payment p = new Payment();
-        p.setId(id);
-        p.setUserId(userId);
-        p.setProject(project);
-        p.setAmount(amount);
-        paymentFacade.create(p);
-        return "Transazione avvenuta con successo";
+    public String addPayment(String id, String userId, long project, double amount) {
+        
+        //controlla che l'id della transazione non sia già presente nel DB
+        for (Payment p : paymentFacade.findAll()) {
+            if (p.getId().equals(id)) {
+                return "Errore: Donazione già effettuata";
+            }
+        }
+        
+        //chiamata al servizio REST di SnaporazSpring per controllo di errori e aggiornamento del progetto
+        Client client = ClientBuilder.newClient();
+        final String url = "http://localhost:8080/SnaporazSpring/donate";
+        WebTarget target = client.target(url);
+        Form form = new Form();
+        form.param("transactionId", id);
+        form.param("project", "" + project);
+        form.param("sum", "" + amount);
+        form.param("idTokenString", userId);
+        JsonObject response = target.request(MediaType.APPLICATION_JSON).post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE), JsonObject.class);
+        
+        //creazione della transazione e inserimento nel DB in caso di successo, segnalazione dell'errore altrimenti
+        if (response.getBoolean("success", false)) {
+            Payment p = new Payment();
+            p.setId(id);
+            p.setUserId(userId);
+            p.setProject(project);
+            p.setAmount(amount);
+            paymentFacade.create(p);
+            return "Transazione avvenuta con successo";
+        } else {
+            return response.getString("response", "Donazione fallita");
+        }
     }
 }
